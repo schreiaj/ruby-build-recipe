1eae461c93c8e441eda1f9f6baf23ed2f8ff23d9
Andrew Schreiber <aschreiber@mitre.org>
2013-09-06 17:37:58 -0400

    Fixed so it pulls a certain commit. Chef revision only pulls branches

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,13 +19,15 @@ git "ruby-build" do
 	repository "https://github.com/sstephenson/ruby-build.git"
 	action :sync
 	destination "/tmp/ruby-build"
-	revision "993b7c2b2d3e43fdb6e439ecf7f260687554f689"
 end
 
 bash "install ruby-build" do
 	user node["user"]
 	cwd "/tmp/ruby-build"
 	code <<-EOH
+		# This is dirty, because Chef doesn't allow us a way to specify a commit I clone
+		# using git provider but then checkout the specific branch I need. 
+		git checkout 993b7c2b2d3e43fdb6e439ecf7f260687554f689
 		sudo ./install.sh
 		cd ..
 		sudo rm -rf ruby-build
